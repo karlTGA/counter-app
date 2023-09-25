a7a96ee38d8bad666df8dd20c3c50540197e10f5
Karl <karl.wolffgang@racemap.com>
2023-09-25 14:01:07 +0200

    small changes of the structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,12 @@ import "./App.css";
 function App() {
   let [value] = useState(0);
 
-  // 1. Fix the bug in the function
+  // 1. Fix the bug that the value doesn't change
   const handleIncrement = () => {
     value += value;
   };
 
-  // 2. Make it possible to set a manual value
-  // (optional: what is the correct type of event variable)
-  // @ts-expect-error
-  const handleInputChange = (event) => {};
+  // 2. add and use a function that increment only with even numbers. type the function in- and output correct.
 
   return (
     <div className="app">
@@ -20,19 +17,14 @@ function App() {
         <h1>Counter App:</h1>
         <div className="current-value-container">
           Current Value
-          <input
-            disabled
-            type="text"
-            value={value}
-            onChange={handleInputChange}
-          />
+          <input disabled type="text" value={value} />
         </div>
 
         <button className="increment-button" onClick={handleIncrement}>
           Increment
         </button>
 
-        {/* 3. Add a red reset button with an confirm dialog */}
+        {/* 3. Optional: Add a red reset button with an confirm dialog */}
       </header>
     </div>
   );
