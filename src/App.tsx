2233c1e01f168531ca4ab0845af37d9d0bd9eea3
Karl <karl.wolffgang@racemap.com>
2023-09-25 14:23:05 +0200

    add a dump mistake

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 
-function App() {
-  let [value] = useState(0);
+// TODOs:
+// - increase only with even numbers
+// - add a red reset button that set the value back to 0
 
-  // 1. Fix the bug that the value doesn't change
+function App() {
+  let [value] = useState(0;
+
   const handleIncrement = () => {
     value += value;
   };
-
-  // 2. add and use a function that increment only with even numbers. type the function in- and output correct.
 
   return (
     <div className="app">
@@ -23,8 +24,6 @@ function App() {
         <button className="increment-button" onClick={handleIncrement}>
           Increment
         </button>
-
-        {/* 3. Optional: Add a red reset button with an confirm dialog */}
       </header>
     </div>
   );
