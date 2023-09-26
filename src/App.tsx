080c0b62988f1167b83f7db7f9e7a979ae400553
EClp007 <erikcmaier@gmail.com>
2023-09-26 15:25:37 +0200

    fixed typo in useState, removed disabled attribute from input, added reset button, increase with only even numbers

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,34 @@ import "./App.css";
 // - add a red reset button that set the value back to 0
 
 function App() {
-  let [value] = useState(0;
+  let [value, setValue] = useState(0);
 
   const handleIncrement = () => {
-    value += value;
+    if(value % 2 == 0) {
+      setValue(value + 2);
+    }
   };
 
-  return (
-    <div className="app">
-      <header className="app-header">
-        <h1>Counter App:</h1>
-        <div className="current-value-container">
-          Current Value
-          <input disabled type="text" value={value} />
-        </div>
+  const handleReset = () => {
+    setValue(0);
+  }
 
-        <button className="increment-button" onClick={handleIncrement}>
-          Increment
-        </button>
-      </header>
-    </div>
+  return (
+      <div className="app">
+        <header className="app-header">
+          <h1>Counter App:</h1>
+          <div className="current-value-container">
+            Current Value
+            <input type="text" value={value}/>
+          </div>
+          <button className="increment-button" onClick={handleIncrement}>
+            Increment
+          </button>
+          <button className="reset-button" onClick={handleReset}>
+            Reset
+          </button>
+        </header>
+      </div>
   );
 }
 
